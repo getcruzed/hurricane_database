2f1469374567f9fb10d879f645f8fd204a13288c
getcruzed <tshinson@gmail.com>
2015-12-15 23:08:24 -0500

    initial set up

---
+++ Hurricane_Project/src/tc_data/TropicalCyclone.java
@@ -0,0 +1,19 @@
+package tc_data;
+
+public class TropicalCyclone {
+	
+	/**
+	 * This will maintain the object of each storm. 
+	 * 
+	 * Each storm will possess these base classes:
+	 * -Hour (normalized by genesis time (hour 0 + 6x)
+	 */
+	
+	
+	/**
+	 * Attributes for TropicalCyclones
+	 */
+	
+	
+
+}
